d1de55e6ee6b824f7c44dd0aa8b13dcee0797654
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-06-23 19:20:22 +0600

    V1.0

---
+++ Q12.1.5/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Q12._1._5
+{
+    class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+    }
+}
