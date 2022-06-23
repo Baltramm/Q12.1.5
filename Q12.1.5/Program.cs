e42c6d4a03de37d8215604c4862acb13c17e8b94
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-06-23 19:08:11 +0600

    Добавьте файлы проекта.

---
+++ Q12.1.5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Q12._1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
