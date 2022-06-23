eb7aa0f347edb094c55e7dcccedeafca74899e39
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-06-23 22:31:16 +0600

    V1.2

--- Q12.1.5/Program.cs
+++ Q12.1.5/Program.cs
@@ -7,20 +7,29 @@ namespace Q12._1._5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+			User[] users = { new User("UserLogin1", "UserName1", true),
+				             new User("UserLogin2", "UserName2", false),
+				             new User("UserLogin3", "UserName3", true),
+				             new User("UserLogin4", "UserName4", false)};
+
+			foreach (User user in users) 
+			{
+				Console.WriteLine("Приветствую,{0}",user.Name);
+				Thread.Sleep(1000);
+				if (!user.IsPremium)
+			    {
+					ShowAds();
+                }
+			}
+
+		}
 		static void ShowAds()
 		{
 			Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
-			// Остановка на 1 с
 			Thread.Sleep(1000);
-
 			Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
-			// Остановка на 2 с
 			Thread.Sleep(2000);
-
 			Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
-			// Остановка на 3 с
 			Thread.Sleep(3000);
 		}
 	}
--- Q12.1.5/User.cs
+++ Q12.1.5/User.cs
@@ -9,5 +9,13 @@ namespace Q12._1._5
         public string Login { get; set; }
         public string Name { get; set; }
         public bool IsPremium { get; set; }
+
+        public User(string login, string name, bool isPremium)
+        {
+            Login = login;
+            Name = name;
+            IsPremium = isPremium;
+        }
     }
+  
 }
